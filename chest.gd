fddf9f18d6aa0bc448be05974064479e98eaa99e
kevinolm10 <kevinolm10@gmail.com>
2026-04-02 15:47:10 +0200

    Meow

--- chest.gd
+++ chest.gd
@@ -1,36 +1,31 @@
 extends Node3D
 
 @onready var zone = $Area3D
-@onready var anim = $chest2/AnimationPlayer
+@onready var anim = $CryptCHObj/AnimationPlayer
 
 var player_nearby = false
 var is_open = false
-var is_closed = false
 
 func _ready():
 	zone.body_entered.connect(_on_body_entered)
 	zone.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body):
+	print(body.name)
 	if body.is_in_group("player"):
 		player_nearby = true
-		print("Player entered chest zone")
-		# TODO: show interaction prompt UI here
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		player_nearby = false
-		print("Player left chest zone")
-		# TODO: hide interaction prompt UI here
 
 func _process(delta):
 	if player_nearby and Input.is_action_just_pressed("interact"):
+		print("Opening chest")
 		open_chest()
 
 func open_chest():
 	if is_open:
-		is_open = false
-		anim.play("close")
-	else:
-		is_open = true
-		anim.play("open")
+		return
+	is_open = true
+	anim.play("lid_001Action")
